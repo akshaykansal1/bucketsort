68658adec8e6ad38a18233cc9e4abcfa9e8ef68e
Richard Thatcher <thatcherrw@gmail.com>
2014-10-24 23:01:25 -0600

    changed int to uint64_t.

--- parallel_bucketsort.c
+++ parallel_bucketsort.c
@@ -6,35 +6,35 @@
 #include <prand.h>
 
 /* 
- * Sequential bucketsort for randomly generated integers.
+ * Sequential bucketsort for randomly generated uint64_tegers.
  *
  */
 
 #define VERBOSE 1
 
-const int DEBUG_LEVEL = DEBUG;
-int count_array_grows = 0;
+const uint64_t DEBUG_LEVEL = DEBUG;
+uint64_t count_array_grows = 0;
 
 int compareTo(const void *, const void *);
 
-const int TRUE = 1;
-const int FALSE = 0;
+const uint64_t TRUE = 1;
+const uint64_t Fnum_listLSE = 0;
 
-static uint64_t min_range,max_range,box_share; 
-static int pid,nproc,n;
-int *A;
-int **bucket; // bucket[i] = array holding the ith bucket
-int *capacity; // capacity[i] = capacity of ith bucket
-int *size; // size[i] = next free location in ith bucket
+static uint64_t min_range,max_range,box_share,n; 
+static int	pid,nproc;
+static uint64_t *num_list;
+static uint64_t **bucket; // bucket[i] = array holding the ith bucket
+static uint64_t *capacity; // capacity[i] = capacity of ith bucket
+static uint64_t *size; // size[i] = next free location in ith bucket
 
-void checkIfSorted(int *array, int n){
-	int i;
-	int sorted;
+void checkIfSorted(uint64_t *array, uint64_t n){
+	uint64_t i;
+	uint64_t sorted;
 
 	sorted = TRUE;
 	for (i=0; i<n-1; i++){
 		if (array[i] > array[i+1]) {
-				sorted = FALSE;
+				sorted = Fnum_listLSE;
 				break;
 		}
 	}
@@ -53,46 +53,46 @@ void checkIfSorted(int *array, int n){
 /*
  * Generate n numbers using the given seed
  */
-void generateInput(int *A, int n, long int seed){
-	int i;
+void generateInput(uint64_t *num_list, uint64_t n, uint64_t seed){
+	uint64_t i;
 
 	srandom(seed);
 	for (i=0; i<n; i++) {
-		A[i] = random();
+		num_list[i] = random();
 	}
 }
 
 
 /*
- * Print the array, one element per line
+ * Pruint64_t the array, one element per line
  */
-void printArray(int *A, int n){
-	int i;
+void printnum_listrray(uint64_t *num_list, uint64_t n){
+	uint64_t i;
 
 	for (i=0; i<n; i++){
-		printf(" %16d \n", A[i]);
+		printf(" %16"PRIu64" \n", num_list[i]);
 	}
 }
 
 
 /*
  *
- * Insert a given value into the specified bucket
+ * Insert a given value uint64_to the specified bucket
  */
 
-void insertInBucket(int value, int bucketIndex){
-	int *tmp;
+void insertInBucket(uint64_t value, uint64_t bucketIndex){
+	uint64_t *tmp;
 
 	if (size[bucketIndex] == capacity[bucketIndex]){
 		//grow the bucket array
-		tmp = (int *) malloc(sizeof(int)*(2*capacity[bucketIndex]));
-		memcpy(tmp, bucket[bucketIndex], capacity[bucketIndex]*sizeof(int));
+		tmp = malloc(sizeof(uint64_t)*(2*capacity[bucketIndex]));
+		memcpy(tmp, bucket[bucketIndex], capacity[bucketIndex]*sizeof(uint64_t));
 		free(bucket[bucketIndex]);
 		bucket[bucketIndex] = tmp;
 		capacity[bucketIndex] = 2 * capacity[bucketIndex];
 		count_array_grows++;
 		if (DEBUG_LEVEL >= 1) {
-			fprintf(stderr, "Growing bucket %d from %d to %d elements\n",
+			fprintf(stderr, "Growing bucket %"PRIu64" from %"PRIu64" to %"PRIu64" elements\n",
 					bucketIndex, capacity[bucketIndex]/2, capacity[bucketIndex]);
 		}
 	}
@@ -107,7 +107,7 @@ void insertInBucket(int value, int bucketIndex){
  * returns  -ve if *x < *y, 0 if *x == *y, +ve if *x > *y
  */
 int compareTo(const void *x, const void *y){
-	return ((*(int *)x) - (*(int *)y));
+	return ((*(uint64_t *)x) - (*(uint64_t *)y));
 }
 
 
@@ -115,30 +115,30 @@ int compareTo(const void *x, const void *y){
  * Sort indiviual bucket using quick sort from std C library
  */
 
-void sortEachBucket(int numBuckets){
-	int i;
+void sortEachBucket(uint64_t numBuckets){
+	uint64_t i;
 
 	for (i=0; i<numBuckets; i++)
 	{
-		qsort(bucket[i], size[i], sizeof(int), compareTo);
+		qsort(bucket[i], size[i], sizeof(uint64_t), compareTo);
 	}
 	if (DEBUG_LEVEL >= 2)
 		for (i=0; i<numBuckets; i++) {
-			fprintf(stderr, "bucket %d has %d elements\n", i, size[i]);
+			fprintf(stderr, "bucket %"PRIu64" has %"PRIu64" elements\n", i, size[i]);
 		}
 }
 
 
 /* 
- * Combine all buckets back into the original array to finish the sorting
+ * Combine all buckets back uint64_to the original array to finish the sorting
  *
  */
-void combineBuckets(int *A, int n, int numBuckets){
-	int i;
+void combineBuckets(uint64_t *num_list, uint64_t n, uint64_t numBuckets){
+	uint64_t i;
 
-	int start = 0;
+	uint64_t start = 0;
 	for (i=0; i<numBuckets; i++) {
-		memcpy(A+start, bucket[i], sizeof(int)*size[i]);
+		memcpy(num_list+start, bucket[i], sizeof(uint64_t)*size[i]);
 		start = start + size[i];
 		free(bucket[i]);
 	}
@@ -147,47 +147,47 @@ void combineBuckets(int *A, int n, int numBuckets){
 
 /*
  * Use bucketsort to sort n uniformly distributed numbers in the range [0..2^31-1].
- * Input: int *A: array of ints A[0..n-1]
- *        int n: number of elements in the input array
- *        int numBuckets: number of buckets to use
+ * Input: uint64_t *num_list: array of uint64_ts num_list[0..n-1]
+ *        uint64_t n: number of elements in the input array
+ *        uint64_t numBuckets: number of buckets to use
  *
  */
 
-void parallelBucketsort(int *A, int n, int numBuckets){
-	int share;
-	int i;
-	int bucketRange;
-	int bucketIndex;
+void parallelBucketsort(uint64_t *num_list, uint64_t n, uint64_t numBuckets){
+	uint64_t share;
+	uint64_t i;
+	uint64_t bucketRange;
+	uint64_t bucketIndex;
 
 	share = n / numBuckets;
 	share = share + (share * 11)/100; // 11% extra for overflow
 
-	capacity = (int *) malloc(sizeof(int)*numBuckets);
-	size = (int *) malloc(sizeof(int)*numBuckets);
-	bucket = (int **) malloc(sizeof(int *)* numBuckets);
+	capacity = (uint64_t *) malloc(sizeof(uint64_t)*numBuckets);
+	size = (uint64_t *) malloc(sizeof(uint64_t)*numBuckets);
+	bucket = (uint64_t **) malloc(sizeof(uint64_t *)* numBuckets);
 
 	for (i=0; i<numBuckets; i++) {
-		bucket[i] = (int *) malloc(sizeof(int)*share);
+		bucket[i] = (uint64_t *) malloc(sizeof(uint64_t)*share);
 		capacity[i] = share;
 		size[i] = 0;
 	}
 
 	bucketRange = RAND_MAX/numBuckets;
 	for (i=0; i<n; i++){
-		bucketIndex = A[i]/bucketRange;
+		bucketIndex = num_list[i]/bucketRange;
 		if (bucketIndex > numBuckets - 1)
 				bucketIndex = numBuckets - 1;
-		insertInBucket(A[i], bucketIndex);
+		insertInBucket(num_list[i], bucketIndex);
 	}
 
 	sortEachBucket(numBuckets);
-	combineBuckets(A, n, numBuckets);
+	combineBuckets(num_list, n, numBuckets);
 	free(capacity);
 	free(size);
 }
 
 
-void print_usage(char * program){
+void pruint64_t_usage(char * program){
 	fprintf(stderr, "Usage %s <n, must be > 1> <#buckets, must between 1 and n> <random seed>\n", program);
 }
 
@@ -208,14 +208,13 @@ void gen_ranges(){
 }
 
 int main(int argc, char **argv){
-	int numBuckets;
-	unsigned int seed;
+	uint64_t numBuckets;
+	uint64_t seed;
 	uint64_t start_time,total_time;
-	uint64_t *num_list;
         MPI_Status status;
 
 	if (argc != 4) {
-		print_usage(argv[0]);
+		pruint64_t_usage(argv[0]);
 		exit(1);
 	}
 
@@ -236,37 +235,37 @@ int main(int argc, char **argv){
 	}
 	num_list = malloc(sizeof(uint64_t)*box_share);
 	gen_ranges();
-	int x;
+	uint64_t x;
 	for(x=0;x<box_share;x++){
 		uint64_t temp = random();
-		
+			
 		num_list[x] = temp;
 	}
 	if ((numBuckets < 1) || (n < 1) || (n < numBuckets)) {
-		print_usage(argv[0]);
+		pruint64_t_usage(argv[0]);
 		exit(1);
 	}
 			
-	A = (int *) malloc(sizeof(int) * n);
+	num_list = (uint64_t *) malloc(sizeof(uint64_t) * n);
 
-	generateInput(A, n, seed);
+	generateInput(num_list, n, seed);
  	if (DEBUG_LEVEL >= 3){ 
-		printArray(A,n);
+		printnum_listrray(num_list,n);
 	}
 
-	parallelBucketsort(A, n, numBuckets);
-	checkIfSorted(A, n);
+	parallelBucketsort(num_list, n, numBuckets);
+	checkIfSorted(num_list, n);
 	if (DEBUG_LEVEL >= 1){
-		printf("Number of array grows is %d\n", count_array_grows);
+		printf("Number of array grows is %"PRIu64"\n", count_array_grows);
 	}
  	if (DEBUG_LEVEL >= 3) {
- 		printArray(A,n);
+ 		printnum_listrray(num_list,n);
 	}
 	total_time = start_time - MPI_Wtime();
-	printf("bucketsort: n = %d  m = %d buckets seed = %d time = %"PRIu64" seconds\n",
+	printf("bucketsort: n = %"PRIu64"  m = %"PRIu64" buckets seed = %"PRIu64" time = %"PRIu64" seconds\n",
 		   n, numBuckets, seed,	total_time);
 
-	free(A);
+	free(num_list);
 
 	MPI_Finalize();
 	exit(0);
